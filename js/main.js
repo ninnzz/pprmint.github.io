1cfa0919b31c0a9899ba03e93ea4cd38ee10e9ff
pprmint <nreclarin@gmail.com>
2014-09-14 15:18:37 +0800

    added new css and scrolling

--- js/main.js
+++ js/main.js
@@ -12,26 +12,78 @@ var available_cmd = {
 					str += '<span class="white">' + i + '</span> : ';
 					str += available_cmd[i].u;
 					str += '  (' + available_cmd[i].m + ")<br/>";
-				} 
+				}
 
-				return str;
+				write_result(str, 1); 	
 			}
 		},
 		whoami : {
-			m : "Displays the description of the user."
+			m : "Displays the description of the user.",
+			e : 1,
+			u : 'whoami',
+			f : function () {
+				write_result('<br/> The description about me here <br/><br/>', 1);
+			}
+		},
+		clear : {
+			m : "Clears the terminal view",
+			e : 1,
+			u : 'clear',
+			f : function () {
+				var i = 0,
+					c = document.getElementById('commands');
+
+				while (c.firstChild) {
+					if (c.childNodes.length === 2) {
+						break;
+					} else {
+						c.removeChild(c.firstChild);
+					}
+				}
+				write_result(false, 0);
+			}
+		},
+		showbabies : {
+			m : "Displays the list of projects.",
+			e : 2,
+			u : 'showbabies [all | github | others] (defaults to all)',
+			f : function () {
+
+
+			}
+
 		}
 	},
 	command_history = [],
 	actual_command = [],
 	curr_command = 0,
 	cursor_pointed = 1,
+	write_result = function (str, type) {
+		var dv = document.createElement('div');
+
+		switch(type) {
+			case 3:
+				dv.innerHTML = '<div class="command-item"><span>&nbsp;-bash cannot ' + str + ': Command not found</span></div>';	
+				break;
+
+			case 2:
+				dv.innerHTML = '<div class="command-item"><span>&nbsp;-bash usage: ' + str + '</span></div>';	
+				break;
+
+			case 1:
+				dv.innerHTML = '<div class="command-item result">' + str + '<br/></div>';
+				break;
+		}	
+		if (str) {
+			document.getElementById("commands").insertBefore(dv, document.getElementById("command"));
+		}
+	},
 	keydown_callback = function (e) {
 		var rc = document.getElementById("root-command"),
 			chr = e.keyCode || e.charCode,
 			span = document.createElement("span"),
 			uncapture = [8, 46, 37, ,38, 39, 40, 13];	
 		
-
 		if (!~uncapture.indexOf(chr)) {
 			cursor_pointed++;
 			if (chr === 32) {
@@ -118,18 +170,17 @@ var available_cmd = {
 				cds = cmd.trim().split(" ");
 				if (available_cmd[cds[0].trim()]) {
 					if (available_cmd[cds[0].trim()].e < cds.length) {
-						dv2.innerHTML = '<div class="command-item"><span>&nbsp;-bash usage: ' + available_cmd[cds[0].trim()].u + '</span></div>';
+						write_result(available_cmd[cds[0].trim()].u, 2);
 					} else {
 						st = available_cmd[cds[0].trim()].f();
-						dv2.innerHTML = '<div class="command-item result">' + st + '<br/></div>';
 					}
+				} else if (cds[0] === "") {
+
 				} else {
-					dv2.innerHTML = '<div class="command-item"><span>&nbsp;-bash cannot ' + cds[0] + ': Command not found</span></div>';
-
+					write_result(cds[0], 3)
 				}
-				document.getElementById("commands").insertBefore(dv2, document.getElementById("command"));
 			}
-
+			document.getElementById( 'command' ).scrollIntoView();
 	};
 
 
@@ -141,6 +192,9 @@ var available_cmd = {
 
     document.getElementById("cmd-txt").addEventListener("keydown", function (e) {
     	var chr = e.keyCode || e.charCode;
+
+		document.getElementById( 'command' ).scrollTop = document.getElementById( 'command' ).scrollHeight;
+
     	if (chr === 38) {
     		e.preventDefault();
     		return;
