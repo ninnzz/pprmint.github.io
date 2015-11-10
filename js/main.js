5f28b6bdb9ad580d7376228fa37ae85bec0e892f
ninz <nreclarin@gmail.com>
2015-11-10 21:46:18 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 /** JS FILE **/
 
 var available_cmd = {
+        ninipot:{
+            m: "Just for my gf's birthday!"
+            e: 1,
+            u: 'ninipot',
+            f: function() {
+                write_result(
+                    '<br/> I am but a <em class="white">shell</em> of my true self, if you want to find the answer, you just need to <em class="white">look inside</em>!!!!!! <br/>');
+            }
+        },
         help : {
             m : "Displays the list of all available commands.",
             e : 1,
