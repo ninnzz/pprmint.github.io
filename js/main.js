0c931f5e7202ce9a32a9769d3bac78337f4b87e2
ninz <nreclarin@gmail.com>
2015-11-10 21:50:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var available_cmd = {
             u: 'ninipot',
             f: function() {
                 write_result(
-                    '<br/> I am but a <em class="white">shell</em> of my true self, if you want to find the answer, you just need to <em class="white">look inside</em>!!!!!! <br/>');
+                    '<br/> <p>I am but a <em class="white">shell</em> of my true self, if you want to find the answer, you just need to <em class="white">look inside</em>!!!!!! </p><br/>');
             }
         },
         help : {
